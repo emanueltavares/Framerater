c57ca61dbfa62e791b2080ac43c02f9bea6b33c0
Emanuel Tavares <emanuelmaues@gmail.com>
2019-09-10 22:56:34 -0300

    Add stress test

---
+++ Assets/Scripts/Stress Test/Neutron.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framerater.StressTest
+{
+    public class Neutron : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+
+        public float AttractionForce { get; set; }
+
+        protected virtual void FixedUpdate()
+        {
+            _rigidbody.AddForce(transform.localPosition * -AttractionForce);
+        }
+    }
+}
---
+++ Assets/Scripts/Stress Test/NucleonSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framerater.StressTest
+{
+    public class NucleonSpawner : MonoBehaviour
+    {
+        [SerializeField] private float _spawnTimeInterval = 1f;
+        [SerializeField] private float _spawnDistance = 1f;
+        [SerializeField] private Neutron[] _prefabs = new Neutron[0];
+        [SerializeField] private float _minAttractionForce = 10f;
+        [SerializeField] private float _maxAttractionForce = 20f;
+
+        private float _timeSinceLastSpawn = 0f;
+
+        protected virtual void OnEnable()
+        {
+            _timeSinceLastSpawn = 0f;
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            if (Time.realtimeSinceStartup - _timeSinceLastSpawn >= _spawnTimeInterval)
+            {
+                SpawnNeutron();
+                _timeSinceLastSpawn = Time.realtimeSinceStartup;
+            }
+        }
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, _spawnDistance);
+        }
+
+        private void SpawnNeutron()
+        {
+            int rndIdex = Random.Range(0, _prefabs.Length);
+            Neutron randomNeutronPrefab = _prefabs[rndIdex];
+            Neutron neutronInstance = Instantiate(randomNeutronPrefab, randomNeutronPrefab.transform.position, randomNeutronPrefab.transform.rotation, transform);
+            neutronInstance.transform.localPosition = Random.insideUnitSphere * _spawnDistance;
+            neutronInstance.AttractionForce = Random.Range(_minAttractionForce, _maxAttractionForce);
+        }
+    }
+
+}
