193bba6b48e98ec4935cef95c5db20eb20718f11
Emanuel Tavares <emanuelmaues@gmail.com>
2019-09-12 20:17:03 -0300

    Change pitch accordingly with the FPS

---
+++ Assets/Scripts/Stress Test/FrameratePitch.cs
@@ -0,0 +1,33 @@
+﻿using Framerater.Core;
+using UnityEngine;
+
+namespace Framerater.StressTest
+{
+    public class FrameratePitch : MonoBehaviour
+    {
+#pragma warning disable CS0649
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private float _targetFramerate = 60f;
+#pragma warning restore CS0649
+
+        private IFramerate _framerater;
+
+        protected virtual void OnEnable()
+        {
+            if (_framerater == null)
+            {
+                _framerater = GetComponent<IFramerate>();
+            }
+        }
+
+        protected virtual void Update()
+        {
+            float pitch = 1f;
+            if (_framerater.NumFrames <= _targetFramerate)
+            {
+                pitch = Mathf.InverseLerp(0f, _targetFramerate, _framerater.NumFrames);
+            }
+            _audioSource.pitch = pitch;
+        }
+    }
+}
