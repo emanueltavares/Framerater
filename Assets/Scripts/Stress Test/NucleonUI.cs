b625f072e4493e0899138a35f176534a7750bdbf
Emanuel Tavares <emanuelmaues@gmail.com>
2019-09-13 00:28:27 -0300

    Add Reset Scene button

---
+++ Assets/Scripts/Stress Test/NucleonUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Framerater.StressTest
+{
+    public class NucleonUI : MonoBehaviour
+    {
+        public void ResetScene()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
